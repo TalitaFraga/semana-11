aab4059a93a3baac1ed58b4e1a1c169ddc6827ee
TalitaFraga <talitad.fraga@gmail.com>
2020-10-21 22:08:14 -0300

    post e delete

--- src/controllers/musicasController.js
+++ src/controllers/musicasController.js
@@ -16,6 +16,41 @@ const getById = (req, res) => {
     
 }
 
+const postMusicas = (req, res) => {
+    console.log(req.body)
+    const { id, title, duration, launchYear, favorited, artists } = req.body
+    musicas.push({ id, title, duration, launchYear, favorited, artists })
+
+    fsWriteFile(res)
+
+    res.status(201).send(musicas)
+
+}
+
+const deleteMusicas = (req, res) => {
+    try {
+        const id = req.params.id
+        const musicaFiltrada = musicas.filter(musica => musica.id == id )
+
+        fsWriteFile(res)
+    
+    
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ message: "Erro ao deletar música"})
+
+    }
+} 
 
 
-module.exports = { getAll, getById }
+module.exports = { getAll, getById, postMusicas, deleteMusicas }
+
+
+
+const fsWriteFile = (res) => fs.writeFile("./src/models/musicas.json", JSON.stringify(musicas), 'utf8', function(err) {
+    if (err) {
+        console.log(err)
+        return res.status(424).send({ message: err })
+    }
+    console.log("Arquivo atualizado com sucesso!")
+})
--- src/routes/musicasRoutes.js
+++ src/routes/musicasRoutes.js
@@ -4,5 +4,7 @@ const controller = require ("../controllers/musicasController")
 
 router.get("/", controller.getAll)
 router.get("/:id", controller.getById)
+router.post("/", controller.postMusicas)
+router.delete("/:id", controller.deleteMusicas)
 
 module.exports = router
